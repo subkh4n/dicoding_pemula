1e99d923b5245be55e061908b0bd58d5a53a2bc5
Novel <mahdibfg123@gmail.com>
2022-11-30 12:47:17 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// import 'detail_screen.dart';
+import 'main_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const MainScreen(),
+    );
+  }
+}
